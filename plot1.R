748ee03245d0bbcae8c2a6707823d31930dbddaa
José Manuel Teles Louro da Silva <JoseLouro@gmail.com>
2015-03-12 15:21:50 +0100

    Signed-off-by: José Manuel Teles Louro da Silva <JoseLouro@gmail.com>

---
+++ plot1.R
@@ -0,0 +1,64 @@
+##########################################################################################################
+## Coursera Course Project 2 for Exploratory Data Analysis
+## José Manuel Teles Louro da Silva 
+## JoseLouro@gmail.com
+
+### plot1.R File Description:
+
+### The overall goal of this assignment is to explore the National Emissions Inventory database and see what it say about fine particulate matter pollution in the United states over the 10-year period 1999–2008. You may use any R package you want to support your analysis.
+### Dataset: Data for Peer Assessment [29Mb]
+### Description: The data for this assignment are available from the course web site as a single zip file:
+### The zip file contains two files:
+### PM2.5 Emissions Data (summarySCC_PM25.rds): This file contains a data frame with all of the PM2.5 emissions data for 1999, 2002, 2005, and 2008. For each year, the table contains number of tons of PM2.5 emitted from a specific type of source for the entire year. Here are the first few rows.
+### 
+### 
+### 
+### 
+### 
+### 
+### 
+### 
+### 
+### 
+##########################################################################################################
+### Set working directory to the location where the Electric power consumption Dataset was unzipped.
+# setwd("H:/My Documents/Personal Stuff/Career Docs/2. Data Science Specialization/4. Exploratory Data Analysis/3. Quizzes/ExData-Project2")
+
+
+# Check for Data File Archive
+fileName <- "NEI_data.zip"
+if(! file.exists(fileName)) {
+	 message("Downloading the data set archive...")
+	 fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+	 download.file(url=fileURL,destfile=fileName,method="curl")
+}
+# Extract Data File
+if!(file.exists("summarySCC_PM25.rds") && file.exists("Source_Classification_Code.rds"))) 
+	 message("Extracting the data set files from the archive...")
+	 unzip(zipfile = fileName)
+}
+
+# Read the NEI & SCC data sets.
+message("Reading the data set files...")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Aggregate the total emissions by year
+message("Aggregate the total emissions by year...")
+aggTotalYear <- aggregate(Emissions ~ year,NEI, sum)
+
+# Plot the Data!
+message("Plotting the data...")
+
+png("plot1.png",width=480,height=480,units="px",bg="transparent")
+barplot(
+  (aggTotalYear$Emissions)/10^6,
+  names.arg=aggTotalYear$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources"
+)
+
+message("Saving the plot...")
+dev.off() # Close the PNG device!
+
